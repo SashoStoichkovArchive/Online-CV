26b21cdf95768c914fd45fbdab8a5446729b4f02
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-03-09 21:12:22 +0200

    trying to fix attempt 22

--- index.php
+++ index.php
@@ -9,25 +9,25 @@
 ?>
 
 <?php
-    foreach (glob("dist/*.html") as $filename){
-        include_once $filename;
-    }
-
-    // foreach (glob("dist/*.ico") as $filename){
+    // foreach (glob("dist/*.html") as $filename){
     //     include_once $filename;
     // }
 
-    foreach (glob("dist/css/*.css") as $filename){
-        include_once $filename;
-    }
+    // // foreach (glob("dist/*.ico") as $filename){
+    // //     include_once $filename;
+    // // }
 
-    foreach (glob("dist/js/*.js") as $filename){
-        include_once $filename;
-    }
+    // foreach (glob("dist/css/*.css") as $filename){
+    //     include_once $filename;
+    // }
 
-    foreach (glob("dist/images/*.+(png|jpg|jpeg|gif|svg)") as $filename){
-        include_once $filename;
-    }
+    // foreach (glob("dist/js/*.js") as $filename){
+    //     include_once $filename;
+    // }
+
+    // foreach (glob("dist/images/*.+(png|jpg|jpeg|gif|svg)") as $filename){
+    //     include_once $filename;
+    // }
 ?>
 
-<?php // include_once("home.html"); ?>
+<?php include_once("home.html"); ?>
