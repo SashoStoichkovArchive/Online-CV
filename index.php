86c334f5a9d8f8b50c5fd22626b4544772aebd3d
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-03-09 20:21:07 +0200

    trying to fix attempt 8

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
     include_once("dist/index.html");
-    // include_once("dist/css/styles.min.css");
-    // include_once("dist/js/scripts.min.js");
+    include_once("dist/css/");
+    include_once("dist/js/");
 
     // foreach (glob("dist/images/*") as $filename){
     //     include_once($filename);
     // }
 ?>
+
+<?php // include_once("home.html"); ?>
