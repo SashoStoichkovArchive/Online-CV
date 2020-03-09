3eb8ed18303366cad196589a0ba321dd8187082d
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-03-09 20:07:33 +0200

    trying to fix attempt 4

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-    foreach (glob("dist/**/*") as $filename) {
+    include_once("dist/index.html");
+    include_once("dist/css/styles.min.css");
+    include_once("dist/js/scripts.min.js");
+
+    foreach (glob("dist/images/*") as $filename){
         include $filename;
     }
 ?>
