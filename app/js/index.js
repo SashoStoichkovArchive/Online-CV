e2562da65bdbc489cd27a96b16b425e80bad93b4
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-03-09 19:53:33 +0200

    add landing page

---
+++ app/js/index.js
@@ -0,0 +1,7 @@
+function toggle(){
+  var sec = document.getElementById('sec');
+  sec.classList.toggle('active');
+
+  var nav = document.getElementById('navigation');
+  nav.classList.toggle('active');
+}
